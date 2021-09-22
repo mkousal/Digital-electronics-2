982ad9dcf31100373a7a8d915535619899f915ce
student <student@PC-637.urel.feec.vutbr.cz>
2021-09-22 12:01:41 +0200

    Initialize project in atmel studio

---
+++ Labs/01-tools/led/led/main.c
@@ -0,0 +1,18 @@
+/*
+ * led.c
+ *
+ * Created: 22.09.2021 11:56:30
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
