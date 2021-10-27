d57524b14abaee6e9483ab97258ea3ab30923027
mkousal <mkousal@gmail.com>
2021-10-27 12:48:59 +0200

    Add finished school work to Labs06

--- Labs/06-lcd/06-lcd/06-lcd/main.c
+++ Labs/06-lcd/06-lcd/06-lcd/main.c
@@ -19,7 +19,16 @@
 #include "timer.h"          // Timer library for AVR-GCC
 #include "lcd.h"            // Peter Fleury's LCD library
 #include <stdlib.h>         // C library. Needed for conversion function
-#include "util/delay.h"
+#include "util/delay.h"		// AVR delay library
+#include "customChars.h"	// Custom LCD characters defines
+
+void lcdUploadCustomChars()
+{
+	lcd_command(1 << LCD_CGRAM);
+	for (uint8_t j = 0; j != sizeof(customChar); j++)
+		lcd_data(customChar[j]);
+
+}
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
@@ -32,6 +41,8 @@ int main(void)
 {
     // Initialize LCD display
     lcd_init(LCD_DISP_ON);
+	
+	lcdUploadCustomChars();
 
     // Put string(s) at LCD display
     lcd_gotoxy(0, 0);
@@ -42,12 +53,25 @@ int main(void)
 	_delay_us(750);
 	lcd_gotoxy(1, 0);
 	lcd_puts("00:00.0");
+	lcd_gotoxy(11, 0);
+	lcd_putc('a');
+	lcd_gotoxy(1, 1);
+	lcd_putc('b');
+	lcd_putc(0);
+	lcd_gotoxy(11, 1);
+	lcd_putc('c');
 	_delay_ms(2000);
 
     // Configure 8-bit Timer/Counter2 for Stopwatch
     // Set the overflow prescaler to 16 ms and enable interrupt
 	TIM2_16m();
 	TIM2_overflow_interrupt_enable();
+	
+	TIM0_overflow_16ms();
+	TIM0_overflow_interrupt_enable();
+	
+	TIM1_overflow_262ms();
+	TIM1_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -72,15 +96,12 @@ void lcdUpdateNumber(uint8_t num, uint8_t pos)
 		lcd_gotoxy(1 + pos*3, 0);
 		if (pos == 2)
 		{
-// 			lcd_gotoxy(1 + pos*3, 0);
 			lcd_putc(lcd_str[0]);
 		}
 		else
 		{
-// 			lcd_gotoxy(2 + pos*3, 0);
 			lcd_putc('0');
 			lcd_putc(lcd_str[0]);
-			
 		}
 	} 
 	else
@@ -93,6 +114,16 @@ void lcdUpdateNumber(uint8_t num, uint8_t pos)
 	}
 }
 
+void lcdShowSquareValue(uint8_t a)
+{
+	char buff[4] = "    ";
+	lcd_gotoxy(11, 0);
+	lcd_puts("    ");
+	lcd_gotoxy(11, 0);
+	itoa(a*a, buff, 10);
+	lcd_puts(buff);
+}
+
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************
  * Function: Timer/Counter2 overflow interrupt
@@ -103,7 +134,6 @@ ISR(TIMER2_OVF_vect)
 {
     static uint8_t number_of_overflows = 0;
 	static uint8_t tens, secs, mins = 0;
-	char lcd_string[2] = "  ";
 	
     number_of_overflows++;
     if (number_of_overflows >= 6)
@@ -111,47 +141,69 @@ ISR(TIMER2_OVF_vect)
         // Do this every 6 x 16 ms = 100 ms
         number_of_overflows = 0;
 		tens++;
-		
 		lcdUpdateNumber(tens, 2);
-// 		itoa(tens, lcd_string, 10);
-// 		lcd_gotoxy(6, 0);
-// 		lcd_puts(lcd_string);
-
+		
 		if (tens == 10)
 		{
 			tens = 0;
 			secs++;
-// 			itoa(tens, lcd_string, 10);
-// 			lcd_gotoxy(6, 0);
-// 			lcd_puts(lcd_string);
 			lcdUpdateNumber(tens, 2);
-// 			itoa(secs, lcd_string, 10);
-// 			lcd_gotoxy(3, 0);
-// 			lcd_puts(lcd_string);
 			lcdUpdateNumber(secs, 1);
+			lcdShowSquareValue(secs);
 			if (secs == 60)
 			{
 				mins++;
 				secs = 0;
-// 				itoa(secs, lcd_string, 10);
-// 				lcd_gotoxy(3, 0);
-// 				lcd_puts(lcd_string);
-// 				itoa(mins, lcd_string, 10);
-// 				lcd_gotoxy(0,0);
-// 				lcd_puts(lcd_string);
 				lcdUpdateNumber(secs, 1);
 				lcdUpdateNumber(mins, 0);
 			}
+			
 			if (mins == 60)
 			{
 				mins = 0;
-// 				itoa(mins, lcd_string, 10);
-// 				lcd_gotoxy(0,0);
-// 				lcd_puts(lcd_string);
 				lcdUpdateNumber(mins, 0);
 			}
 		}
 		
     }
     // Else do nothing and exit the ISR
+}
+
+ISR(TIMER0_OVF_vect) // Runs every 16 ms, 6*16 = 96ms (:=) 100ms
+{
+	static uint8_t pos, sym = 0;
+	lcd_gotoxy(1 + pos, 1);
+	lcd_putc(sym);
+	sym++;
+	if(sym == 6)
+	{
+		sym = 0;
+		pos++;
+		if (pos == 10)
+		{
+			pos = 0;
+			lcd_gotoxy(1, 1);
+			lcd_puts("          ");
+		}
+	}
+}
+
+ISR(TIMER1_OVF_vect) // Runs every 262 ms, coordinates for c = (11, 1)
+{
+	static uint8_t running_text[] = "   I like Digital Electronics\n";
+	static uint8_t cnt, cnt1 = 0;
+	cnt++;
+	if (cnt == 4)
+	{
+		cnt = 0;
+		cnt1++;
+		lcd_gotoxy(11, 1);
+		for (uint8_t i = cnt1; i != cnt1 + 5; i++)
+		{
+			lcd_putc(running_text[i]);
+			if (running_text[i] == 10)
+				cnt1 = 0;
+		}
+
+	}
 }
